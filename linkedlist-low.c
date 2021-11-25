58ea3221f299eb92160542938b46c5b47e23237c
cleverly_kk <82257405+cleverlykk@users.noreply.github.com>
2021-11-25 22:16:48 +0530

    Update linkedlist-low.c

--- linkedlist-low.c
+++ linkedlist-low.c
@@ -1,99 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct node
-{
-    int data;
-    struct node *next;
-};
-void main()
-    {
-        struct node *head;
-
-        int arr[] = {40, 10, 60, 1, 5, 15, 50};
-        for (int i = 0; i<6; i++)
-        {
-            struct node *ptr;
-            ptr = (struct node *)malloc(sizeof(struct node *));
-            ptr->data = arr[i];
-            ptr->next = head;
-            head = ptr;
-        }
-        int display()
-        {
-            struct node *ptr;
-            ptr = head;
-            if (ptr == NULL)
-            {
-                printf("Nothing to print");
-            }
-            else
-            {
-                printf("\nprinting values . . . . .\n");
-                while (ptr != NULL)
-                {
-                    printf("\n%d", ptr->data);
-                    ptr = ptr->next;
-                }
-            }
-            return 0;
-        }
-
-        int randominsert(){
-            int i, loc, item;
-            struct node *ptr, *temp;
-            ptr = (struct node *)malloc(sizeof(struct node));
-            if (ptr == NULL)
-            {
-                printf("\nOVERFLOW");
-            }
-            else
-            {
-                printf("\nEnter element value");
-                scanf("%d", &item);
-                ptr->data = item;
-                printf("\nEnter the location after which you want to insert ");
-                scanf("\n%d", &loc);
-                temp = head;
-                for (i = 0; i < loc; i++)
-                {
-                    temp = temp->next;
-                    if (temp == NULL)
-                    {
-                        printf("\ncan't insert\n");
-                        return;
-                    }
-                }
-                ptr->next = temp->next;
-                temp->next = ptr;
-                printf("\nNode inserted");
-            }
-            return 0;
-        }
-        int random_delete()
-        {
-            struct node *ptr, *ptr1;
-            int loc, i;
-            printf("\n Enter the location of the node after which you want to perform deletion \n");
-            scanf("%d", &loc);
-            ptr = head;
-            for (i = 0; i < loc; i++)
-            {
-                ptr1 = ptr;
-                ptr = ptr->next;
-
-                if (ptr == NULL)
-                {
-                    printf("\nCan't delete");
-                    return;
-                }
-            }
-            ptr1->next = ptr->next;
-            free(ptr);
-            printf("\nDeleted node %d ", loc + 1);
-            return 0;
-        }
-        display();
-        randominsert();
-        random_delete();
-        display();
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+void main(){
+    int size,last=0;
+    
+    int arr[100];
+    int data[]={30,50,20,10,45,65,5};
+    last=6;
+    for (int i=0;i<=6;i++){
+        arr[i]=data[i];
     }
+    void insert(int pos,int val){
+        if(last>size){
+            printf("aray is full");
+        }
+        else {
+            for(int i=last;i<pos;i--){
+                arr[i]=arr[i+1];
+            }
+            arr[pos]=val;
+            last++;
+        }
+    }
+    void remove(int pos){
+        if(pos<last){
+            printf("in valid postion");
+        }
+        else{
+            pos-=1;
+            for(int i=pos-1;i<=size;i++){
+                arr[i]=arr[i+1];
+            }
+        }
+    }
+    void display(){
+        for (int i=0;i<last;i++){
+            printf("%d\n",arr[i]);
+        }
+    }
+    void search(int val){
+        for(int i=0;i<last;i++){
+            if (arr[i]==val){
+                printf("element found at %d\n",i+1);
+            }
+        }
+    }
+    insert(2,5);
+    int s;
+    scanf("%d",&s);
+    search(s);
+    display();
+}
